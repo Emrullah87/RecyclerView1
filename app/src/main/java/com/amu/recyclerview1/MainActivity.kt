8a1ede74c19e3762306c267aa64d47ef7c9a5861
Emrullah87 <emrullah.altuntas87@gmail.com>
2025-08-16 19:26:30 +0200

    layoutManager und Adapter definiert

--- app/src/main/java/com/amu/recyclerview1/MainActivity.kt
+++ app/src/main/java/com/amu/recyclerview1/MainActivity.kt
@@ -2,10 +2,24 @@ package com.amu.recyclerview1
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    //Manager arranges the layout of the RecyclerView in your list
+    private var layoutManager: RecyclerView.LayoutManager? = null
+
+    //Adapter binds the data to the views in the RecyclerView
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        layoutManager = LinearLayoutManager(this)
+
+
     }
 }
